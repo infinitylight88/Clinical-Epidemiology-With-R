8a75d6280cccf88b2710ddfe77d74708e160c824
Derrick Napokoli <napokoliderrick88@gmail.com>
2026-03-12 20:17:50 +0300

    started withindexing dataset

---
+++ week01_dataset_simulation/simulate_daaset.R
@@ -0,0 +1,7 @@
+# Clinical Epidemiology with R
+# Week 1: Simulating a cardiometabolic dataset
+# Author: Your Name
+# Sample size: 500 adults
+
+# %03d means: pad with 0, width of 3, integer (d)
+uniqe_id <- sprintf("%03d", 1:500)
---
+++ week01_dataset_simulation/simulate_dataset.r
@@ -0,0 +1,6 @@
+# Clinical Epidemiology with R
+# Week 1: Simulating a cardiometabolic dataset
+# Author: Your Name
+# Sample size: 500 adults
+
+
