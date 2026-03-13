771e6253fd772043222939497e15cff9e8aaba01
Derrick Napokoli <napokoliderrick88@gmail.com>
2026-03-13 19:36:26 +0300

    cleaned the files and removed duplicate files

---
+++ week01_dataset_simulation/simulate_dataset.R
@@ -0,0 +1,11 @@
+# Clinical Epidemiology with R
+# Week 1: Simulating a cardiometabolic dataset
+# Author: Your Name
+# Sample size: 500 adults
+
+# %03d means: pad with 0, width of 3, integer (d)
+uniqe_id <- sprintf("p%03d", 1:500)
+# Quick validation
+length(patient_id)
+head(patient_id)
+tail(patient_id)
--- week01_dataset_simulation/simulate_dataset.r
+++
@@ -1,6 +0,0 @@
-# Clinical Epidemiology with R
-# Week 1: Simulating a cardiometabolic dataset
-# Author: Dettick Napokoli
-# Sample size: 500 adults
-
-
